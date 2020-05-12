37ed289331904469f2f3440924c6620a45c03b2e
darthjee <darthjee@gmail.com>
2020-05-12 10:53:16 +0200

    Add docs

--- lib/jace/executer.rb
+++ lib/jace/executer.rb
@@ -18,6 +18,8 @@ module Jace
     # @param context [Object] Object where the procs / methods
     #   will be called on
     # @block [Proc] bloc to be performed between befores and afters
+    #
+    # @return [Object] result of block call
     def self.call(before: [], after: [], context:, &block)
       new(before, after, context, &block).call
     end
@@ -25,6 +27,8 @@ module Jace
     # calls the execution
     #
     # @see .call
+    #
+    # @return (see .call)
     def call
       execute_actions(before)
       result = block.call
@@ -35,6 +39,9 @@ module Jace
 
     private
 
+    # @private
+    #
+    # @param (see .call)
     def initialize(before, after, context, &block)
       @before = [before].flatten.compact
       @after = [after].flatten.compact
@@ -44,12 +51,30 @@ module Jace
 
     attr_reader :before, :after, :context, :block
 
+    # @private
+    #
+    # Perform actions from list
+    #
+    # @list [Array<Symbol,Proc>] methods and procs to
+    #   be executed
+    #
+    # Execute each proc and call method in the list
+    # within the given context
+    #
+    # @return [Array<Proc>]
     def execute_actions(list)
       actions(list).each do |action|
         context.instance_eval(&action)
       end
     end
 
+    # @private
+    #
+    # Transforms the input object into a Proc
+    #
+    # @param list [Symbol,Proc] method or proc to be transformed
+    #
+    # @return [Proc]
     def actions(list)
       list.map do |entry|
         if entry.is_a?(Proc)
