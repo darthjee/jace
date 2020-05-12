5c29afc83fbf20ed85fcc9f76254dafe61b26d88
Favini <darthjee@gmail.com>
2020-05-12 19:02:33 +0200

    Build and release (#4)

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,6 +18,9 @@ function isTagged() {
 ACTION=$1
 
 case $ACTION in
+  "signin")
+    echo "---\n:rubygems_api_key: $RUBY_GEMS_API_KEY" ~/.gem/credentials
+    ;;
   "build")
     if $(isTagged); then
       rake build
