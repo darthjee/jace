fc04f765c5b6a9b6807a9537aea4bdd3a0664746
darthjee <darthjee@gmail.com>
2022-08-18 18:57:17 -0300

    Add empty spec

---
+++ spec/lib/jace/register_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Jace::Register do
+  subject(:registry) { described_class.new }
+
+  describe '#register' do
+  end
+
+  describe '#trigger' do
+  end
+end
