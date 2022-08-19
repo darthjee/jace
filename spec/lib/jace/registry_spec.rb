4085ccf05ad957f7740122b7a68483ea84f30097
darthjee <darthjee@gmail.com>
2022-08-19 09:56:21 -0300

    Rubocop fix

--- spec/lib/jace/registry_spec.rb
+++ spec/lib/jace/registry_spec.rb
@@ -7,64 +7,81 @@ describe Jace::Registry do
 
   describe '#register' do
     let(:event_name) { :event_name }
+    let(:expected_registry) do
+      { event_name: { after: [Proc] } }
+    end
 
     context 'when event is a symbol' do
       it 'adds even to events registry' do
         expect { registry.register(event_name) {} }
-          .to change { registry.registry }
-          .to({ event_name: { after: [Proc]} })
+          .to change(registry, :registry)
+          .to(expected_registry)
       end
 
       it 'adds event to event list' do
         expect { registry.register(event_name) {} }
-          .to change { registry.events }
+          .to change(registry, :events)
           .by([:event_name])
       end
     end
 
     context 'when event is a string' do
       let(:event_name) { 'event_name' }
+      let(:expected_registry) do
+        { event_name: { after: [Proc] } }
+      end
 
       it 'adds even to events registry' do
         expect { registry.register(event_name) {} }
-          .to change { registry.registry }
-          .to({ event_name: { after: [Proc]} })
+          .to change(registry, :registry)
+          .to(expected_registry)
       end
 
       it 'adds event to event list' do
         expect { registry.register(event_name) {} }
-          .to change { registry.events }
+          .to change(registry, :events)
           .by([:event_name])
       end
     end
 
     context 'when the event was already registerd' do
+      let(:expected_registry) do
+        { event_name: { after: [Proc, Proc] } }
+      end
+
       before { registry.register(event_name) {} }
 
       it 'adds even a callback to the event registry' do
         expect { registry.register(event_name) {} }
-          .to change { registry.registry }
-          .to({ event_name: { after: [Proc, Proc]} })
+          .to change(registry, :registry)
+          .to(expected_registry)
       end
 
       it 'adds event to event list' do
         expect { registry.register(event_name) {} }
-          .not_to(change { registry.events })
+          .not_to(change(registry, :events))
       end
     end
 
     context 'when another event was already registerd' do
+      let(:expected_registry) do
+        {
+          event_name: { after: [Proc] },
+          other_event_name: { after: [Proc] }
+        }
+      end
+
       before { registry.register(:other_event_name) {} }
 
       it 'adds even a callback to the event registry' do
         expect { registry.register(event_name) {} }
-          .to change { registry.registry }
-          .to({ event_name: { after: [Proc] }, other_event_name: { after: [Proc] } })
+          .to change(registry, :registry)
+          .to(expected_registry)
       end
 
       it 'adds event to event list' do
         expect { registry.register(event_name) {} }
-          .to change { registry.events }
+          .to change(registry, :events)
           .by([:event_name])
       end
     end
@@ -72,16 +89,16 @@ describe Jace::Registry do
 
   describe '#trigger' do
     let(:event_name) { :event_name }
-    let(:context)    { double("context") }
+    let(:context)    { double('context') }
 
     context 'when an event handler has been registered' do
       before do
         allow(context).to receive(:method_call)
         allow(context).to receive(:other_method)
-        registry.register(event_name) { method_call  }
+        registry.register(event_name) { method_call }
       end
 
-      it "execute the event handler" do
+      it 'execute the event handler' do
         registry.trigger(event_name, context) {}
         expect(context).to have_received(:method_call)
       end
@@ -98,16 +115,16 @@ describe Jace::Registry do
         allow(context).to receive(:another_method_call)
         allow(context).to receive(:other_method)
 
-        registry.register(event_name) { method_call  }
-        registry.register(event_name) { another_method_call  }
+        registry.register(event_name) { method_call }
+        registry.register(event_name) { another_method_call }
       end
 
-      it "execute the event handler" do
+      it 'execute the event handler' do
         registry.trigger(event_name, context) {}
         expect(context).to have_received(:method_call)
       end
 
-      it "execute the other event handler" do
+      it 'execute the other event handler' do
         registry.trigger(event_name, context) {}
         expect(context).to have_received(:another_method_call)
       end
@@ -122,10 +139,10 @@ describe Jace::Registry do
       before do
         allow(context).to receive(:method_call)
         allow(context).to receive(:other_method)
-        registry.register(:other_event) { method_call  }
+        registry.register(:other_event) { method_call }
       end
 
-      it "does not execute the event handler" do
+      it 'does not execute the event handler' do
         registry.trigger(event_name, context) {}
         expect(context).not_to have_received(:method_call)
       end
