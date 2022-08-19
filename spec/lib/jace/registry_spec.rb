a54f12a5da56eee1acc862633ae927aa5d7926a8
darthjee <darthjee@gmail.com>
2022-08-19 09:46:34 -0300

    Handle other events

--- spec/lib/jace/registry_spec.rb
+++ spec/lib/jace/registry_spec.rb
@@ -92,6 +92,24 @@ describe Jace::Registry do
       end
     end
 
+    context 'when another event handler has been registered' do
+      before do
+        allow(context).to receive(:method_call)
+        allow(context).to receive(:other_method)
+        registry.register(:other_event) { method_call  }
+      end
+
+      it "does not execute the event handler" do
+        registry.trigger(event_name, context) {}
+        expect(context).not_to have_received(:method_call)
+      end
+
+      it 'execute the block' do
+        registry.trigger(event_name, context) { context.other_method }
+        expect(context).to have_received(:other_method)
+      end
+    end
+
     context 'when an event handler has not been registered' do
       before do
         allow(context).to receive(:other_method)
